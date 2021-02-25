21dfcfaaf19d99c8fdbd8692d79a25175d68713d
Yeyuxian <1178688214@qq.com>
2021-02-25 21:01:28 +0800

    feat:write a dictionaries

--- wordcloudjson.py
+++ wordcloudjson.py
@@ -20,3 +20,18 @@ for word in words:
         wordCounts[word] = wordCounts.get(word, 0) + 1
 items = list(wordCounts.items())
 items.sort(key=lambda x: x[1], reverse=True)
+#写入字典
+countList = []
+for i in range(len(items)):
+    countDict = {}
+    word, count = items[i]
+    if count >= 10:
+        countDict['name'] = word
+        countDict['value'] = count
+        countList.append(countDict)
+# print(countList)
+data = {}
+data['data'] = countList
+print(data)
+with open('comments.json', 'w',errors = 'ignore') as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
