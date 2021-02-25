b07a32972c234719424417d4d2fd90cfcd89b3ff
Yeyuxian <1178688214@qq.com>
2021-02-25 20:57:10 +0800

    feat:the preparing work

---
+++ wordcloud.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 25 09:41:16 2021
+
+@author: ASUS
+"""
+
+import requests
+import re
+import time
+#准备工作
+url = 'https://video.coral.qq.com/varticle/5963120294/comment/v2?callback=_varticle5963120294commentv2&orinum=10&oriorder=o&pageflag=1&cursor=0&scorecursor=0&orirepnum=2&reporder=o&reppageflag=1&source=132&_=1614174646181'
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36'}
+html = requests.get(url,headers = headers).content.decode()
+print(html)
+comments_list = list()
+totalcom = int(re.findall('"oritotal":(.*?),',html,re.S)[0])
+print(totalcom)
+rangetime = round((totalcom - 10)/10)+1
+print(rangetime)
+t = int(time.time())
+print(t)
+now = t + 3
+n = 1
+m = 1
+print(now,n,m)
+cursor = 0
+
